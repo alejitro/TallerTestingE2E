d0a4a79bad155be1bb5db83c5e96c038224003b2
Alejandro Martinez <alejitro@gmail.com>
2019-08-25 11:09:18 -0500

    Implementacion de taller protractor

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -33,3 +33,94 @@ describe('Tour of heroes, heroes page', () => {
   });
 
 });
+
+describe('Tour of heroes, search a Hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should search a hero', () => {
+    page.navigateTo();
+    expect(page.searchHero('Bombasto')).toBe('Bombasto');
+  });
+
+});
+
+describe('Tour of heroes, delete a Hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should delete a hero', () => {
+    page.navigateToHeroes();
+    const currentHeroes = page.getAllHeroes().count();
+    page.deleteAHero('15');
+    expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n - 1));
+  });
+
+});
+
+describe('Tour of heroes, edit a Hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should edit a hero', async () => {
+    await page.navigateToHeroeDetails('19');
+    await page.editHero('Crystal');
+    //page.navigateTo();
+    //expect(page.searchHero('Crystal')).toBe('Crystal');
+    /*const currentHeroes = page.getAllHeroes().count();
+    page.deleteAHero('15');
+    expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n - 1));*/
+  });
+
+});
+
+describe('Tour of heroes, go to a Hero from Dashboard', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should go a hero page from Dashboard', () => {
+    page.navigateTo();
+    expect(page.gotoAHeroFromDashboard('Celeritas')).toBe('Celeritas');
+  });
+
+});
+
+describe('Tour of heroes, go to a Hero from Heroes List', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should go a to hero page from HeroList', () => {
+    page.navigateToHeroes();
+    expect(page.gotoAHeroFromList('12')).toBe('Narco');
+  });
+
+});
+
+describe('Tour of heroes, go to a Hero from search', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should go to a hero from search', () => {
+    page.navigateTo();
+    expect(page.gotoAHeroFromSearch('Bombasto')).toBe('Bombasto');
+  });
+
+});
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,4 @@
-import {browser, by, element, ElementFinder} from 'protractor';
+import {browser, by, element, ElementFinder, protractor} from 'protractor';
 
 export class TourOfHeroesPage {
   navigateTo() {
@@ -13,6 +13,10 @@ export class TourOfHeroesPage {
     element(by.linkText('Heroes')).click();
   }
 
+  navigateToHeroeDetails(id: string) {
+    return browser.get('/detail/' + id );
+  }
+
   getAllHeroes() {
     return element(by.tagName('my-heroes')).all(by.tagName('li'));
   }
@@ -21,4 +25,79 @@ export class TourOfHeroesPage {
     element(by.tagName('input')).sendKeys(newHero);
     element(by.buttonText('Add')).click();
   }
+
+  searchHero(hero:string){
+    element(by.id('search-box')).sendKeys(hero);
+    return element(by.css('.search-result')).getText().then(function(text) {
+      browser.sleep(2000);
+      return text;
+    });
+  }
+
+  deleteAHero(heroName: string){
+
+    element.all(by.css('.heroes li')).filter(function(elem, index) {
+      return elem.element(by.css('.badge')).getText().then(function(text) {
+        return text === heroName;
+      });
+    }).first().element(by.tagName('button')).click();
+  }
+
+  editHero(heroName: string){
+    var span1= element(by.css('[placeholder = "name"]'))
+    span1.clear();
+    span1.sendKeys(heroName);
+    browser.sleep(1000);
+    var saveButton = element(by.buttonText('Save'));
+    browser.actions().mouseMove(saveButton).click().perform().then(function(){
+      console.log("Se pulso el boton de guardar");
+      var EC = protractor.ExpectedConditions;
+      // Wait for new page url to contain newPageName
+      browser.wait(EC.urlContains('/heroes'), 10000);
+    });
+    browser.waitForAngular();
+    browser.sleep(3000);
+  }
+
+  gotoAHeroFromDashboard(heroName: string){
+    element.all(by.tagName('h4')).filter(function(elem){
+      return elem.getText().then(function(text){
+        return text === heroName;
+      });
+    }).first().click();
+    browser.waitForAngular();
+    browser.sleep(3000);
+    return element(by.css('[placeholder = "name"]')).getAttribute('ng-reflect-model').then(function(value){
+        return value;
+    });
+  }
+  gotoAHeroFromList(heroName: string){
+    element.all(by.css('.heroes li')).filter(function(elem, index) {
+      return elem.element(by.css('.badge')).getText().then(function(text) {
+        return text === heroName;
+      });
+    }).first().click();
+    element(by.buttonText('View Details')).click();
+    browser.sleep(1000);
+    browser.waitForAngular();
+    browser.sleep(3000);
+    return element(by.css('[placeholder = "name"]')).getAttribute('ng-reflect-model').then(function(value){
+        return value;
+    });
+  }
+
+  gotoAHeroFromSearch(heroName: string){
+    element(by.id('search-box')).sendKeys(heroName);
+    element.all(by.css('.search-result')).filter(function(elem){
+      return elem.getText().then(function(text){
+        return text === heroName;
+      });
+    }).first().click();
+    browser.waitForAngular();
+    browser.sleep(3000);
+    return element(by.css('[placeholder = "name"]')).getAttribute('ng-reflect-model').then(function(value){
+        return value;
+    });
+  }
+
 }
